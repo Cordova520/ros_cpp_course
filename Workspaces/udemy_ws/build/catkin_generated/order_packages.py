e8fb87220b1c083030cc0fb35ce4917dd3a58634
Cordova520 <daniel_cordovag98@hotmail.com>
2022-10-23 02:24:00 -0500

    First ROS package cotaining the project structure

---
+++ Workspaces/udemy_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/cordova520/Documents/Udemy/ros_cpp_course/Workspaces/udemy_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
---
+++ Workspaces/udemy_ws/build/udemy_cpp_pkg/CMakeFiles/publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/cordova520/Documents/Udemy/ros_cpp_course/Workspaces/udemy_ws/devel/lib/udemy_cpp_pkg/publisher"
+  "/home/cordova520/Documents/Udemy/ros_cpp_course/Workspaces/udemy_ws/devel/lib/udemy_cpp_pkg/publisher.pdb"
+  "CMakeFiles/publisher.dir/src/publisher.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

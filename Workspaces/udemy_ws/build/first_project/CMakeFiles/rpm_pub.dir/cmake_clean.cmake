bdd3dad5fcc240f8e14c83cd74f6ec9b7fe3c6bc
Cordova520 <daniel_cordovag98@hotmail.com>
2022-10-24 23:00:43 -0500

    CMake list compiled for first project

---
+++ Workspaces/udemy_ws/build/first_project/CMakeFiles/rpm_pub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/cordova520/Documents/Udemy/ros_cpp_course/Workspaces/udemy_ws/devel/lib/first_project/rpm_pub"
+  "/home/cordova520/Documents/Udemy/ros_cpp_course/Workspaces/udemy_ws/devel/lib/first_project/rpm_pub.pdb"
+  "CMakeFiles/rpm_pub.dir/src/publisher_rpm.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rpm_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Workspaces/udemy_ws/build/first_project/CMakeFiles/speed_pub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/cordova520/Documents/Udemy/ros_cpp_course/Workspaces/udemy_ws/devel/lib/first_project/speed_pub"
+  "/home/cordova520/Documents/Udemy/ros_cpp_course/Workspaces/udemy_ws/devel/lib/first_project/speed_pub.pdb"
+  "CMakeFiles/speed_pub.dir/src/subscriber_speed.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/speed_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

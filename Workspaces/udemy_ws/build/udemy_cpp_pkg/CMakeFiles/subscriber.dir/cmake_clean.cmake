34cbe9435a7ebbac428e2b4f0bf4552fdf671cba
Cordova520 <daniel_cordovag98@hotmail.com>
2022-10-24 21:17:43 -0500

    CMake compiled for the adition of the subscriber

---
+++ Workspaces/udemy_ws/build/udemy_cpp_pkg/CMakeFiles/subscriber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/cordova520/Documents/Udemy/ros_cpp_course/Workspaces/udemy_ws/devel/lib/udemy_cpp_pkg/subscriber"
+  "/home/cordova520/Documents/Udemy/ros_cpp_course/Workspaces/udemy_ws/devel/lib/udemy_cpp_pkg/subscriber.pdb"
+  "CMakeFiles/subscriber.dir/src/subscriber.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
